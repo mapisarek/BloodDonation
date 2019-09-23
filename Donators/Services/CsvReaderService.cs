0b8ac3a53d83e05fcccefb852759b36fdc1ce9a2
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-23 23:59:00 +0200

    Added CSV reader with ICsvReader interface

---
+++ Donators/Services/CsvReaderService.cs
@@ -0,0 +1,43 @@
+﻿using Donators.Models;
+using LumenWorks.Framework.IO.Csv;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Donators.Services
+{
+    public interface ICsvReader
+    {
+        List<string[]> LoadFile(HttpPostedFileBase uploadedFile);
+
+    }
+
+    public class CsvReaderService : ICsvReader
+    {
+
+        private string[] delimiters = { ";", "," };
+        private List<string[]> listOfDonators = new List<string[]>();
+
+        public List<string[]> LoadFile(HttpPostedFileBase upload)
+        {
+            if (upload != null && upload.ContentLength > 0)
+            {
+                if (upload.FileName.EndsWith(".csv"))
+                {
+                    Stream stream = upload.InputStream;
+                    using (CsvReader csvReader =
+                        new CsvReader(new StreamReader(stream), true))
+                    {
+                        listOfDonators = csvReader.ToList();
+                    }
+                    
+                }
+            }
+            return listOfDonators;
+        }
+
+    }
+}
