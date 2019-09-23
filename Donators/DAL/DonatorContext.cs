de26b48379f4dfbd54a4e7cb9d1f00c4648bf400
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-23 23:54:04 +0200

    Added Donator EF context

---
+++ Donators/DAL/DonatorContext.cs
@@ -0,0 +1,18 @@
+﻿using Donators.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Donators.DAL
+{
+    public class DonatorContext : DbContext
+    {
+        public DonatorContext() : base("DonatorContext")
+        {
+        }
+
+        public DbSet<Donator> Donators { get; set; }
+    }
+}
